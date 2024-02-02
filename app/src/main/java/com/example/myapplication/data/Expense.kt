9eb836ba78d3b76512d534edea4adf7706143238
milantru1 <milantru1@gmail.com>
2024-02-02 12:19:28 +0100

    Add created date to Expense

--- app/src/main/java/com/example/myapplication/data/Expense.kt
+++ app/src/main/java/com/example/myapplication/data/Expense.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.math.BigDecimal
+import java.time.LocalDate
 
 @Entity(tableName = "expense_table")
 data class Expense(
@@ -10,5 +11,6 @@ data class Expense(
     val id: Int,
     val value: BigDecimal,
     val note: String,
+    val created: LocalDate,
     val categories: List<Category>
 )
