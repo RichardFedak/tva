cfb676b5bbe35462fda0b6cba1cc58f4b97f309b
RichardF <richardfedak@gmail.com>
2024-02-02 14:29:39 +0100

    Implement SharedViewModel

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.viewmodels.SharedViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var sharedViewModel: SharedViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Calendar())
+        sharedViewModel = ViewModelProvider(this)[SharedViewModel::class.java]
+
+
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.bottom_nav_calendar -> replaceFragment(Calendar())
@@ -21,6 +30,13 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        sharedViewModel.selectedDate.observe(this, Observer { date ->
+            // Handle the selected date change
+            val spendingDetailFragment = SpendingDetail.newInstance(date.toString())
+            replaceFragment(spendingDetailFragment)
+        })
+
 
     }
 
