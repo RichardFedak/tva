56ec0f5ff4a081f3422f222302ed3c074c0d9d3a
RichardF <richardfedak@gmail.com>
2024-02-02 10:46:44 +0100

    Bottom bar navigation added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Calendar())
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.bottom_nav_calendar -> replaceFragment(Calendar())
+                R.id.bottom_nav_stats -> replaceFragment(Stats())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        var fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
