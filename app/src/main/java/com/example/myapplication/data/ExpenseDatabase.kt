e5b1b183cdc975a259afa183b8c0ae264d51a3a3
RichardF <richardfedak@gmail.com>
2024-03-12 13:40:34 +0100

    Hotfix

--- app/src/main/java/com/example/myapplication/data/ExpenseDatabase.kt
+++ app/src/main/java/com/example/myapplication/data/ExpenseDatabase.kt
@@ -8,7 +8,8 @@ import androidx.room.TypeConverters
 
 @Database(
     entities = [Expense::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class ExpenseDatabase: RoomDatabase() {
